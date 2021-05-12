5c1d2149978f61939bc92413e9e150d10b4e1c4d
Craig Neff <ifly12610@gmail.com>
2021-05-11 22:20:42 -0600

    messing around with using a React router with a Switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
+  const posts = [
+    {
+      id: 1,
+      title: "My first Post",
+      date: "4-7-2020",
+      content: "This is my first post!",
+    },
+    {
+      id: 2,
+      title: "Checking In",
+      date: "4-8-2020",
+      content: "Yesterday was a good day, looking forward to another!",
+    },
+    {
+      id: 3,
+      title: "Vacation Time",
+      date: "4-9-2020",
+      content: "Finally time to head to Denver for our trip",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/posts">
+            <Posts posts={posts} />
+          </Route>
+          <Route path="/friends">
+            <Friends names={["Tom", "Sally", "Samantha"]} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
