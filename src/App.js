fa3f26b612e064902af5269fdde2095ebc80d3ba
Craig Neff <ifly12610@gmail.com>
2021-05-13 22:05:18 -0600

    Still working on router inside a router. Still need to do this a few more times

--- src/App.js
+++ src/App.js
@@ -77,6 +77,44 @@ function Friends(props) {
   );
 }
 
-function Posts() {
-  return <h2>Posts</h2>;
+function Posts({ posts }) {
+  const match = useRouteMatch();
+  const findPostById = (id) => posts.filter((post) => post.id == id)[0];
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((post, index) => {
+          return (
+            <li key={index}>
+              <Link to={`${match.url}/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Switch>
+        <Route
+          path={`${match.path}/:postId`}
+          render={(props) => (
+            <Post {...props} data={findPostById(props.match.params.postId)} />
+          )}
+        />
+        <Route path={match.path}>
+          <h3>Please select a Post.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Post(props) {
+  const { data } = props;
+  return (
+    <div>
+      <h3>{data.title}</h3>
+      <h4>{data.date}</h4>
+      <p>{data.content}</p>
+    </div>
+  );
 }
